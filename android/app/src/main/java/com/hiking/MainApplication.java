ce66d9300ab5c130153fbb90079185dcdbb54907
Unknown <blast.off2010@hotmail.com>
2018-05-28 16:19:41 +0200

    Fixed duplicate declarations + override

--- android/app/src/main/java/com/hiking/MainApplication.java
+++ android/app/src/main/java/com/hiking/MainApplication.java
@@ -8,67 +8,13 @@ import com.reactlibrary.RNUUIDGeneratorPackage;
 import com.horcrux.svg.SvgPackage;
 import com.github.xinthink.rnmk.ReactMaterialKitPackage;
 import com.airbnb.android.react.maps.MapsPackage;
-import com.imagepicker.ImagePickerPackage;
-import com.evollu.react.fcm.FIRMessagingPackage;
-import com.kishanjvaghela.cardview.RNCardViewPackage;
-import org.reactnative.camera.RNCameraPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.reactlibrary.RNUUIDGeneratorPackage;
-import com.horcrux.svg.SvgPackage;
-import com.github.xinthink.rnmk.ReactMaterialKitPackage;
-import com.airbnb.android.react.maps.MapsPackage;
-import com.imagepicker.ImagePickerPackage;
-import com.evollu.react.fcm.FIRMessagingPackage;
-import com.kishanjvaghela.cardview.RNCardViewPackage;
-import org.reactnative.camera.RNCameraPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.reactlibrary.RNUUIDGeneratorPackage;
-import com.horcrux.svg.SvgPackage;
-import com.github.xinthink.rnmk.ReactMaterialKitPackage;
-import com.airbnb.android.react.maps.MapsPackage;
-import com.imagepicker.ImagePickerPackage;
-import com.evollu.react.fcm.FIRMessagingPackage;
-import com.kishanjvaghela.cardview.RNCardViewPackage;
-import org.reactnative.camera.RNCameraPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.reactlibrary.RNUUIDGeneratorPackage;
-import com.horcrux.svg.SvgPackage;
-import com.github.xinthink.rnmk.ReactMaterialKitPackage;
-import com.airbnb.android.react.maps.MapsPackage;
-import com.imagepicker.ImagePickerPackage;
-import com.evollu.react.fcm.FIRMessagingPackage;
-import com.kishanjvaghela.cardview.RNCardViewPackage;
-import org.reactnative.camera.RNCameraPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.reactlibrary.RNUUIDGeneratorPackage;
-import com.horcrux.svg.SvgPackage;
-import com.github.xinthink.rnmk.ReactMaterialKitPackage;
-import com.airbnb.android.react.maps.MapsPackage;
-import com.imagepicker.ImagePickerPackage;
-import com.evollu.react.fcm.FIRMessagingPackage;
-import com.kishanjvaghela.cardview.RNCardViewPackage;
-import org.reactnative.camera.RNCameraPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.reactlibrary.RNUUIDGeneratorPackage;
-import com.horcrux.svg.SvgPackage;
-import com.github.xinthink.rnmk.ReactMaterialKitPackage;
-import com.airbnb.android.react.maps.MapsPackage;
-import com.imagepicker.ImagePickerPackage;
-import com.evollu.react.fcm.FIRMessagingPackage;
-import com.kishanjvaghela.cardview.RNCardViewPackage;
-import org.reactnative.camera.RNCameraPackage;
-import org.reactnative.camera.RNCameraPackage;
-import com.reactlibrary.RNUUIDGeneratorPackage;
-import com.github.xinthink.rnmk.ReactMaterialKitPackage;
-import com.horcrux.svg.SvgPackage;
-import com.imagepicker.ImagePickerPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.kishanjvaghela.cardview.RNCardViewPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import com.imagepicker.ImagePickerPackage;
 import com.evollu.react.fcm.FIRMessagingPackage;
-import com.airbnb.android.react.maps.MapsPackage;
+import com.kishanjvaghela.cardview.RNCardViewPackage;
+import org.reactnative.camera.RNCameraPackage;
 import com.facebook.soloader.SoLoader;
 
 import java.util.Arrays;
@@ -94,61 +40,7 @@ public class MainApplication extends Application implements ReactApplication {
             new ImagePickerPackage(),
             new FIRMessagingPackage(),
             new RNCardViewPackage(),
-            new RNCameraPackage(),
-            new VectorIconsPackage(),
-            new RNUUIDGeneratorPackage(),
-            new SvgPackage(),
-            new ReactMaterialKitPackage(),
-            new MapsPackage(),
-            new ImagePickerPackage(),
-            new FIRMessagingPackage(),
-            new RNCardViewPackage(),
-            new RNCameraPackage(),
-            new VectorIconsPackage(),
-            new RNUUIDGeneratorPackage(),
-            new SvgPackage(),
-            new ReactMaterialKitPackage(),
-            new MapsPackage(),
-            new ImagePickerPackage(),
-            new FIRMessagingPackage(),
-            new RNCardViewPackage(),
-            new RNCameraPackage(),
-            new VectorIconsPackage(),
-            new RNUUIDGeneratorPackage(),
-            new SvgPackage(),
-            new ReactMaterialKitPackage(),
-            new MapsPackage(),
-            new ImagePickerPackage(),
-            new FIRMessagingPackage(),
-            new RNCardViewPackage(),
-            new RNCameraPackage(),
-            new VectorIconsPackage(),
-            new RNUUIDGeneratorPackage(),
-            new SvgPackage(),
-            new ReactMaterialKitPackage(),
-            new MapsPackage(),
-            new ImagePickerPackage(),
-            new FIRMessagingPackage(),
-            new RNCardViewPackage(),
-            new RNCameraPackage(),
-            new VectorIconsPackage(),
-            new RNUUIDGeneratorPackage(),
-            new SvgPackage(),
-            new ReactMaterialKitPackage(),
-            new MapsPackage(),
-            new ImagePickerPackage(),
-            new FIRMessagingPackage(),
-            new RNCardViewPackage(),
-            new RNCameraPackage(),
-            new RNCameraPackage(),
-            new ImagePickerPackage(),
-            new RNUUIDGeneratorPackage(),
-            new ReactMaterialKitPackage(),
-            new SvgPackage(),
-            new VectorIconsPackage(),
-            new FIRMessagingPackage(),
-            new RNCardViewPackage(),
-            new MapsPackage()
+            new RNCameraPackage()
       );
     }
 
